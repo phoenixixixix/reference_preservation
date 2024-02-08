357261fec6795cb61ac2640be157cd97c7647519
phoenixixixix <barchukoleg12@gmail.com>
2024-02-08 21:15:44 +0200

    🍏 PWA implementation:
    * Add service_worker to make able to use app when offline;
    * Add manifest file to define UI specifications;
    * Add rails controller to serve service_worker and manifest files;
    * Implement companion - the bridge between service worker and rails;

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  get "/service-worker.js" => "service_worker#service_worker"
+  get "/manifest.json" => "service_worker#manifest"
+
   resources :links, except: %i[ show ]
   resources :groups, except: %i[ show ]
+
   root "links#index"
 end
